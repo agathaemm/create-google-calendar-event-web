3617c6cfe726e7f242d6839cde4cb86216a30284
Agatha Menezes <agathaemm@gmail.com>
2024-07-03 14:50:48 -0300

    feat: add remove function

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,22 @@ import { useGoogleCalendar } from './hooks/useGoogleCalendar'
 
 
 function App() {
-  const {handleCreateEvent} = useGoogleCalendar()
+  const {handleCreateEvent, handleRemoveEvent, eventsList} = useGoogleCalendar()
 
   return (
     <>
       <h1>POC Criar evento na agenda do Google</h1>
       <div className="card">
-        <button onClick={handleCreateEvent}>criar evento</button>
+        <button onClick={handleCreateEvent}>Criar evento</button>
       </div>
+
+      <h3>Meus eventos</h3>
+      {eventsList?.map((event) => (
+        <div key={event.id} style={{display: 'flex', gap: 10, alignItems: 'center'}}>
+          <p>{event.start.dateTime || event.start.date} - {event.summary}</p>
+          <button style={{height: 40}} onClick={() => handleRemoveEvent(event.id)}>Apagar</button>
+      </div>
+      ))}
     </>
   )
 }
--- src/hooks/useGoogleCalendar.ts
+++ src/hooks/useGoogleCalendar.ts
@@ -3,6 +3,7 @@ import { event } from "../mock";
 
 export const useGoogleCalendar = () => {
   const [tokenClient, setTokenClient] = useState<any>(null)
+  const [eventsList, setEventsList] = useState<Array<any>>([])
 
   useEffect(() => {
     if(gapi) {
@@ -24,6 +25,11 @@ export const useGoogleCalendar = () => {
     setTokenClient(myTokenClient)
   }, [google])
 
+  useEffect(() => {
+    if(!tokenClient) return;
+    handleListEvents()
+  }, [tokenClient])
+
   async function createEvent() {
     let response: any;
     try {
@@ -37,6 +43,61 @@ export const useGoogleCalendar = () => {
     }
 
     window.alert(`Evento criado com sucesso! ${response.result.htmlLink}`)
+  }
+
+  async function removeEvent(eventId: string) {
+    let response: any;
+    try {
+      response = await gapi.client.calendar.events.delete({
+        calendarId: 'primary',
+        eventId: eventId
+      })
+    } catch (err) {
+      console.log('erro: ', err)
+      return;
+    }
+
+    window.alert(`Evento removido com sucesso!`)
+  }
+
+  async function listEvents() {
+    let response: any;
+    try {
+      response = await gapi.client.calendar.events.list({
+        calendarId: 'primary',
+        timeMin: new Date().toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime',
+      });
+      console.log('response: ', response)
+      const events = response.result.items
+      if(!events || events.length === 0) {
+        return []
+      }
+      return events
+    }catch(err) {
+      console.log('erro: ', err)
+      return;
+    }
+  }
+
+  function handleRemoveEvent(eventId: string) {
+    tokenClient.callback = async (resp: any) => {
+      if (resp.error !== undefined) {
+        throw (resp);
+      }
+      await removeEvent(eventId);
+    };
+
+    if (gapi.client.getToken() === null) {
+      // Prompt the user to select a Google Account and ask for consent to share their data
+      // when establishing a new session.
+      tokenClient.requestAccessToken({prompt: 'consent'});
+    } else {
+      // Skip display of account chooser and consent dialog for an existing session.
+      tokenClient.requestAccessToken({prompt: ''});
+    }
   }
 
   function handleCreateEvent() {
@@ -57,5 +118,24 @@ export const useGoogleCalendar = () => {
     }
   }
 
-  return {handleCreateEvent}
+  function handleListEvents() {
+    tokenClient.callback = async (resp: any) => {
+      if (resp.error !== undefined) {
+        throw (resp);
+      }
+      const events = await listEvents();
+      return setEventsList(events)
+    };
+
+    if (gapi.client?.getToken() === null) {
+      // Prompt the user to select a Google Account and ask for consent to share their data
+      // when establishing a new session.
+      tokenClient.requestAccessToken({prompt: 'consent'});
+    } else {
+      // Skip display of account chooser and consent dialog for an existing session.
+      tokenClient.requestAccessToken({prompt: ''});
+    }
+  }
+
+  return {handleCreateEvent, handleRemoveEvent, handleListEvents, eventsList}
 }
